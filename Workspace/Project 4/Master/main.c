dda09f1a59750e820fd9a32caa033a0a96a9cc0d
Garidy <garidy@sandersonline.org>
2023-03-05 16:02:00 -0700

    Master send I2C and Slave 1 turn on LEDbar

--- Workspace/Project 4/Master/main.c
+++ Workspace/Project 4/Master/main.c
@@ -1,12 +1,67 @@
 #include <msp430.h> 
 
-
-/**
- * main.c
- */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+
+	//setup B0 for I2C
+	UCB0CTLW0 |= UCSWRST;   // put in SW RST
+	UCB0CTLW0 |= UCSSEL_3;  // choose SMCLK
+	UCB0BRW = 10;           // set prescalar to 10
+
+	UCB0CTLW0 |= UCMODE_3;  // put into I2C mode
+	UCB0CTLW0 |= UCMST;     // set as master
+	UCB0CTLW0 |= UCTR;      // default Tx mode
+	UCB0I2CSA = 0x68;       // set slave address
+
+	UCB0CTLW1 |= UCASTP_2;   // auto STOP mode
+	UCB0TBCNT |= 1;         // count = 1 byte
+
+    //-- setup ports
+	P1SEL1 &= ~BIT3;   //P1.3 SCL (48)
+	P1SEL1 |= BIT3;
+
+    P1SEL1 &= ~BIT2;   //P1.2 SDA (1)
+    P1SEL1 |= BIT2;
+
+    //test leds
+
+    P1DIR |= BIT0;  //config LED 1 to output
+    P6DIR |= BIT6;  //config LED 2 to output
+    P1OUT &= ~BIT0; //clear LED 1
+    P6OUT &= ~BIT6; //clear LED 2
+
+    PM5CTL0 &= ~LOCKLPM5;
+    UCB0CTLW0 &= ~UCSWRST;   // take B0 out of SW RST
+
+
+	//enable B0 TX0 IRQ
+	UCB0IE |= UCTXIE0;  //local enable for TX0
+	__enable_interrupt();   //enable maskables
+
+	int i;
+
+    while(1){
+        P1OUT |= BIT0;
+        P6OUT &= ~BIT6;
+
+        UCB0CTLW0 |= UCTXSTT;   //manually start
+
+        for(i=0; i<100; i=i+1){}//delay
+
+    }
+    return 0;
+
 }
+
+
+//--------------------------------------------
+//--ISRs
+#pragma vector = EUSCI_B0_VECTOR
+__interrupt void EUSCI_B0_I2C_ISR(void)
+{
+    UCB0TXBUF = 0xBB;
+
+}
+
+
--- Workspace/Project 4/Slave1/main.c
+++ Workspace/Project 4/Slave1/main.c
@@ -8,5 +8,39 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+    P1DIR |= BIT0;  //config LED 1 to output
+    P1DIR |= BIT1;  //config LED 2 to output
+    P1DIR |= BIT4;  //config LED 3 to output
+    P1DIR |= BIT5;  //config LED 4 to output
+    P1DIR |= BIT6;  //config LED 5 to output
+    P1DIR |= BIT7;  //config LED 6 to output
+    P2DIR |= BIT6;  //config LED 7 to output
+    P2DIR |= BIT7;  //config LED 8 to output
+
+
+
+    P1OUT &= ~BIT0; //clear LED 1
+    P1OUT &= ~BIT1; //clear LED 2
+    P1OUT &= ~BIT4; //clear LED 3
+    P1OUT &= ~BIT5; //clear LED 4
+    P1OUT &= ~BIT6; //clear LED 5
+    P1OUT &= ~BIT7; //clear LED 6
+    P2OUT &= ~BIT6; //clear LED 7
+    P2OUT &= ~BIT7; //clear LED 8
+
+    PM5CTL0 &= ~LOCKLPM5;
+
+
+    while(1){
+        P1OUT |= BIT0;  //Set LED 1
+        P1OUT |= BIT1;  //Set LED 2
+        P1OUT |= BIT4;  //Set LED 3
+        P1OUT |= BIT5;  //Set LED 4
+        P1OUT |= BIT6;  //Set LED 5
+        P1OUT |= BIT7;  //Set LED 6
+        P2OUT |= BIT6;  //Set LED 7
+        P2OUT |= BIT7;  //Set LED 8
+
+    }
 	return 0;
 }
