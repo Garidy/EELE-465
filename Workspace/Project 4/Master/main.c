8be466b2e722dcbebae96f8ae817e7db41845649
Garidy <garidy@sandersonline.org>
2023-03-09 19:31:22 -0700

    I2C master to slave communication working

--- Workspace/Project 4/Master/main.c
+++ Workspace/Project 4/Master/main.c
@@ -1,53 +1,66 @@
 #include <msp430.h> 
 
+char Data_In =0;
+char Packet[] = {0x03};
+
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-	//setup B0 for I2C
-	UCB0CTLW0 |= UCSWRST;   // put in SW RST
-	UCB0CTLW0 |= UCSSEL_3;  // choose SMCLK
-	UCB0BRW = 10;           // set prescalar to 10
+    //setup B0 for I2C
+    UCB0CTLW0 |= UCSWRST;   // put in SW RST
+    UCB0CTLW0 |= UCSSEL_3;  // choose SMCLK
+    UCB0BRW = 10;           // set prescalar to 10
 
-	UCB0CTLW0 |= UCMODE_3;  // put into I2C mode
-	UCB0CTLW0 |= UCMST;     // set as master
-	UCB0CTLW0 |= UCTR;      // default Tx mode
-	UCB0I2CSA = 0x68;       // set slave address
+    UCB0CTLW0 |= UCMODE_3;  // put into I2C mode
+    UCB0CTLW0 |= UCMST;     // set as master
+    UCB0CTLW0 |= UCTR;      // default Tx mode
+    UCB0I2CSA = 0x02;       // set slave address
 
-	UCB0CTLW1 |= UCASTP_2;   // auto STOP mode
-	UCB0TBCNT |= 1;         // count = 1 byte
+    UCB0CTLW1 |= UCASTP_2;   // auto STOP mode
+    UCB0TBCNT |= 1;         // count = 1 byte
 
     //-- setup ports
-	P1SEL1 &= ~BIT3;   //P1.3 SCL (48)
-	P1SEL1 |= BIT3;
+    P1SEL1 &= ~BIT3;   //P1.3 SCL (48)
+    P1SEL0 |= BIT3;
 
     P1SEL1 &= ~BIT2;   //P1.2 SDA (1)
-    P1SEL1 |= BIT2;
+    P1SEL0 |= BIT2;
 
     //test leds
 
     P1DIR |= BIT0;  //config LED 1 to output
     P6DIR |= BIT6;  //config LED 2 to output
-    P1OUT &= ~BIT0; //clear LED 1
+    P1OUT &= ~BIT0; //clear LED 1``````
     P6OUT &= ~BIT6; //clear LED 2
 
     PM5CTL0 &= ~LOCKLPM5;
     UCB0CTLW0 &= ~UCSWRST;   // take B0 out of SW RST
 
 
-	//enable B0 TX0 IRQ
-	UCB0IE |= UCTXIE0;  //local enable for TX0
-	__enable_interrupt();   //enable maskables
+    //enable B0 TX0 IRQ
+    UCB0IE |= UCTXIE0;  //local enable for TX0
+    //UCB0IE |= UCRXIE0;  // Rx
+    __enable_interrupt();   //enable maskables
 
-	int i;
+    int i;
 
     while(1){
         P1OUT |= BIT0;
         P6OUT &= ~BIT6;
 
+        //UCB0CTLW0 |= UCTR;      //Tx Mode
         UCB0CTLW0 |= UCTXSTT;   //manually start
+        for(i=0; i<100; i=i+1){}
 
-        for(i=0; i<100; i=i+1){}//delay
+        //while(( UCB0IFG & UCSTPIFG ) == 0){}
+        //    UCB0IFG &= ~UCSTPIFG;   //clear stop flag
+
+        //UCB0CTLW0 &= ~UCTR;      //Rx Mode
+       // UCB0CTLW0 |= UCTXSTT;   //manually start
+
+        //while(( UCB0IFG & UCSTPIFG ) == 0){}
+        //    UCB0IFG &= ~UCSTPIFG;   //clear stop flag
 
     }
     return 0;
@@ -60,8 +73,26 @@ int main(void)
 #pragma vector = EUSCI_B0_VECTOR
 __interrupt void EUSCI_B0_I2C_ISR(void)
 {
-    UCB0TXBUF = 0xBB;
+
+    UCB0TXBUF = 0x33;
+
+    /*
+    switch(UCB0IV){
+    case 0x16:
+        Data_In = UCB0RXBUF;
+        break;
+    case 0x18:
+        UCB0TXBUF = 0x03;
+        break;
+    default:
+        break;
+    }
+    */
+
+  //  UCB0CTLW0 &= ~UCTR;      // Rx mode
+
+   // Data_In = UCB0RXBUF;
+
 
 }
-
 
--- Workspace/Project 4/Slave1/main.c
+++ Workspace/Project 4/Slave1/main.c
@@ -1,13 +1,33 @@
 #include <msp430.h> 
 
+int Data;
 
-/**
- * main.c
- */
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
+    UCB0CTLW0 |= UCSWRST; //RESET
+
+
+	UCB0CTLW0 |= UCMODE_3; //SELECT I2C
+    UCB0I2COA0 = 0x02;     //SLAVE ADDRESS
+    UCB0CTLW0 &= ~UCMST;   //slave MODE
+    UCB0CTLW0 &= ~UCTR;      //RECEIVE MODE
+    //UCB0I2COA0 |= UCGCEN;   // general call response enable
+    UCB0I2COA0 |= UCOAEN;   // slave address enable
+
+    UCB0CTLW0 &= ~UCTR;      //RECEIVE MODE
+    UCB0CTLW1 &= ~UCSWACK;  // auto acknowledge
+    UCB0TBCNT = 0x01;      // Length of Receiving data
+
+    //UCB0CTLW1 &= ~UCTXIFG0;
+    P1SEL1 &= ~BIT3;    //P1.3 = SCL
+    P1SEL0 |= BIT3;
+
+    P1SEL1 &= ~BIT2;     //P1.2 = SDA
+    P1SEL0 |= BIT2;
+
+
     P1DIR |= BIT0;  //config LED 1 to output
     P1DIR |= BIT1;  //config LED 2 to output
     P1DIR |= BIT4;  //config LED 3 to output
@@ -16,8 +36,6 @@ int main(void)
     P1DIR |= BIT7;  //config LED 6 to output
     P2DIR |= BIT6;  //config LED 7 to output
     P2DIR |= BIT7;  //config LED 8 to output
-
-
 
     P1OUT &= ~BIT0; //clear LED 1
     P1OUT &= ~BIT1; //clear LED 2
@@ -28,10 +46,18 @@ int main(void)
     P2OUT &= ~BIT6; //clear LED 7
     P2OUT &= ~BIT7; //clear LED 8
 
+
     PM5CTL0 &= ~LOCKLPM5;
+
+    UCB0CTLW0 &= ~UCSWRST;              // OUT OF RESET
+
+    UCB0IE |= UCRXIE0;                  //ENABLE I2C RX IRQ
+
+    __enable_interrupt();
 
 
     while(1){
+
         P1OUT |= BIT0;  //Set LED 1
         P1OUT |= BIT1;  //Set LED 2
         P1OUT |= BIT4;  //Set LED 3
@@ -41,6 +67,22 @@ int main(void)
         P2OUT |= BIT6;  //Set LED 7
         P2OUT |= BIT7;  //Set LED 8
 
+
     }
 	return 0;
+}
+
+//--------------------------------------------
+//--ISRs
+#pragma vector = EUSCI_B0_VECTOR
+__interrupt void EUSCI_B0_I2C_ISR(void)
+{
+    Data = UCB0RXBUF;
+    /*
+    UCB0IE &= ~UCRXIE0;
+    if(UCB0IE != '\0'){
+        Data = UCB0RXBUF;
+    }
+    UCB0IE |= UCRXIE0;
+    */
 }
