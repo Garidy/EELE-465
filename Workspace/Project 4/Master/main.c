869ae00e014be5b38913f67415873b2ed2c6a7f4
Garidy <garidy@sandersonline.org>
2023-03-16 12:38:34 -0600

    Password Unlock Functionality

--- Workspace/Project 4/Master/main.c
+++ Workspace/Project 4/Master/main.c
@@ -1,9 +1,9 @@
 #include <msp430.h> 
 
 char keypad;
-char Data_In =0;
-char Packet[] = {0x03};
 
+char checkKeypad(void);
+char getChag(char button);
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
@@ -33,38 +33,53 @@ int main(void)
     UCB0CTLW0 &= ~UCSWRST;   // take B0 out of SW RST
 
 
+    P5DIR |= BIT3;
+    P5OUT &= ~BIT3;
+
     //enable B0 TX0 IRQ
     UCB0IE |= UCTXIE0;  //local enable for TX0
     //UCB0IE |= UCRXIE0;  // Rx
     __enable_interrupt();   //enable maskables
 
+
+
     int i;
+    int unlockStatus = 0;
+
+
+    while(unlockStatus != 3){
+        keypad = checkKeypad();
+        if(keypad != 0x00){
+            if(keypad == '1' && unlockStatus == 0){
+                unlockStatus = 1;
+                for(i=0; i<100; i=i+1){}
+            }else if(keypad == '2' && unlockStatus == 1){
+                unlockStatus  = 2;
+                for(i=0; i<100; i=i+1){}
+            }else if(keypad == '3' && unlockStatus == 2){
+                unlockStatus = 3;
+                for(i=0; i<100; i=i+1){}
+                break;
+            }
+        }
+
+    }
+
 
     while(1){
-        P1OUT |= BIT0;
-        P6OUT &= ~BIT6;
-
+        P5OUT |= BIT3;
         keypad = checkKeypad();
 
-        //UCB0CTLW0 |= UCTR;      //Tx Mode
         UCB0CTLW0 |= UCTXSTT;   //manually start
         for(i=0; i<100; i=i+1){}
 
-        //while(( UCB0IFG & UCSTPIFG ) == 0){}
-        //    UCB0IFG &= ~UCSTPIFG;   //clear stop flag
-
-        //UCB0CTLW0 &= ~UCTR;      //Rx Mode
-       // UCB0CTLW0 |= UCTXSTT;   //manually start
-
-        //while(( UCB0IFG & UCSTPIFG ) == 0){}
-        //    UCB0IFG &= ~UCSTPIFG;   //clear stop flag
 
     }
     return 0;
 
 }
 
-char checkKeypad() {
+char checkKeypad(void) {
     char buttonPressed = 0x00;      //init 0
 
     //D1 = P4.1
@@ -114,7 +129,7 @@ char checkKeypad() {
     }else if(P2IN & BIT2){
         buttonPressed = 0x01;
     }else{
-        return '\0';
+        return 0x00;
     };
 
 
@@ -166,10 +181,70 @@ char checkKeypad() {
     }else if(P1IN & BIT6){
         buttonPressed = buttonPressed + 0x10;
     }else{
-        return '\0';
+        return 0x00;
     };
 
-    return buttonPressed;
+    return getChar(buttonPressed);
+
+}
+
+char getChar(char buttonPressed){
+    char button = 0x00;
+
+        switch(buttonPressed) {
+            case 0x11:
+                button = '1';
+                break;
+            case 0x12:
+                button = '2';
+                break;
+            case 0x14:
+                button = '3';
+                break;
+            case 0x18:
+                button = 'A';
+                break;
+
+            case 0x21:
+                button = '4';
+                break;
+            case 0x22:
+                button = '5';
+                break;
+            case 0x24:
+                button = '6';
+                break;
+            case 0x28:
+                button = 'B';
+                break;
+
+            case 0x41:
+                button = '7';
+                break;
+            case 0x42:
+                button = '8';
+                break;
+            case 0x44:
+                button = '9';
+                break;
+            case 0x48:
+                button = 'C';
+                break;
+
+            case 0x81:
+                button = '*';
+                break;
+            case 0x82:
+                button = '0';
+                break;
+            case 0x84:
+                button = '#';
+                break;
+            case 0x88:
+                button = 'D';
+                break;
+        }
+        return button;
 
 }
 
