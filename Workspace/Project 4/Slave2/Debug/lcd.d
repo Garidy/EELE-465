a6e5c502d86d868736826be3d95189af0fbca9a4
Garidy <garidy@sandersonline.org>
2023-03-26 16:19:49 -0600

    re-add project 3/4 files

---
+++ Workspace/Project 4/Slave2/Debug/lcd.d
@@ -0,0 +1,27 @@
+# FIXED
+
+lcd.obj: ../lcd.c
+lcd.obj: ../lcd.h
+lcd.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h
+lcd.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2310.h
+lcd.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h
+lcd.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+lcd.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+lcd.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/legacy.h
+
+../lcd.c:
+
+../lcd.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2310.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/legacy.h:
+
